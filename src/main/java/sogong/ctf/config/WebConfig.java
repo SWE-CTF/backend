2d790aebff84a107450f809f222ce0a76bb049da
jueun oh <jueun1025@naver.com>
2023-10-09 23:45:01 +0900

    [feature] 사용자 인가 using argumentResolver

---
+++ src/main/java/sogong/ctf/config/WebConfig.java
@@ -0,0 +1,16 @@
+package sogong.ctf.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import sogong.ctf.service.CustomMemberResolver;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new CustomMemberResolver());
+    }
+}
--- src/main/java/sogong/ctf/service/MemberService.java
+++ src/main/java/sogong/ctf/service/MemberService.java
@@ -96,5 +96,4 @@ public class MemberService {
         return true;
 
     }
-
 }
