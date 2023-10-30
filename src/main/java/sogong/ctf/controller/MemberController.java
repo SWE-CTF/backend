5842653bfbc2dc0145ec16579d64c78750e08b26
jueun oh <jueun1025@naver.com>
2023-10-30 23:15:51 +0900

    [style] return 수정

--- src/main/java/sogong/ctf/controller/MemberController.java
+++ src/main/java/sogong/ctf/controller/MemberController.java
@@ -5,12 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sogong.ctf.dto.MemberRequestDTO;
+import sogong.ctf.dto.MemberResponseDTO;
 import sogong.ctf.dto.TeamFormDTO;
 import sogong.ctf.service.MemberService;
 import sogong.ctf.service.TeamService;
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.bind.ValidationException;
 import java.security.NoSuchProviderException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,24 +22,29 @@ public class MemberController {
     private final TeamService teamService;
 
     @GetMapping("/api/member/join") //소속 가입할때 가능한 소속 보여주기 , url 미정
-    public ResponseEntity joinForm(){
-        return new ResponseEntity(teamService.findAllTeam(), HttpStatus.OK);
+    public ResponseEntity<List<String>> joinForm(){
+        try {
+            return ResponseEntity.ok(teamService.findAllTeam());
+        }catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PostMapping("/api/admin/team") //url 미정 , 소속 추가 기능
     public ResponseEntity create(@RequestBody TeamFormDTO teamFormDTO){
         try{
             teamService.createTeam(teamFormDTO);
-            return ResponseEntity.status(HttpStatus.OK).body("success");
+            return ResponseEntity.ok().build();
         } catch(Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            return ResponseEntity.badRequest().build();
         }
     }
 
     @PostMapping("/api/member/login")
     public ResponseEntity login(@RequestBody MemberRequestDTO request){
         try{
-            return new ResponseEntity(memberService.login(request),HttpStatus.OK);
+            MemberResponseDTO login = memberService.login(request);
+            return ResponseEntity.ok(login);
         }catch (Exception e){
             return ResponseEntity.badRequest().build();
         }
@@ -46,7 +53,8 @@ public class MemberController {
     @PostMapping("/api/member/join")
     public ResponseEntity join(@RequestBody MemberRequestDTO request){
         try{
-            return new ResponseEntity<>(memberService.join(request), HttpStatus.OK);
+            memberService.join(request);
+            return ResponseEntity.ok().build();
         } catch(Exception e){
             return ResponseEntity.badRequest().build();
         }
